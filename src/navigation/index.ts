48c56d87c6f9d61f7627db75aa413b89bcf4f5d4
Jyoti Ranjan Nayak <32037939+jrnbulu@users.noreply.github.com>
2026-08-01 16:27:58 +0000

    return-delete

--- src/navigation/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Navigation Index
- * 
- * Central export point for all navigation components
- */
-
-export { RootNavigator } from './RootNavigator';
-export { AppNavigator } from './AppNavigator';
